ed3fc0574cdab479140e77c66b62c6fea380c109
AnastasiiaSukha <suha.anastasia@gmail.com>
2023-01-04 16:07:23 +0400

    finished HW04

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Schema, SchemaTypes, model } = require("mongoose");
+const Joi = require("joi")
+
+const { handleMongooseError } = require("../helpers");
+const { string } = require("joi");
+
+const userSchema = new Schema({
+    password: {
+        type: String,
+        required: [true, 'Set password for user'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    subscription: {
+        type: String,
+        enum: ["starter", "pro", "business"],
+        default: "starter"
+    },
+    token: String,
+    owner: {
+        type: SchemaTypes.ObjectId,
+        ref: 'user',
+    },
+}, { versionKey: false, timestamps: true });
+
+userSchema.post('save', handleMongooseError);
+
+const userJoiSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    subscription: Joi.string(),
+})
+
+const User = model("user", userSchema);
+
+module.exports = {
+    User,
+    userJoiSchema,
+}
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-
 const app = require('./app')
 
-const {DB_HOST, PORT = 3000} = process.env;
+mongoose.set('strictQuery', true);
 
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.connect(DB_HOST)
   .then(()=> app.listen(PORT, (err) => console.log("Database connection successful")))
